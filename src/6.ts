b1d91bd058366251f37a9fda7bde80b53354a041
elodierubert802 <elodierubert802@gmail.com>
2025-03-11 16:52:13 +0000

    Add 6.ts

---
+++ src/6.ts
@@ -0,0 +1,11 @@
+class User {
+  private username: string;
+  private email: string;
+  private password: string;
+
+  constructor(username: string, email: string, password: string) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+}
