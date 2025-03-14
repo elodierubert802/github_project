a5df84f45394169440a632464e4d144eec64ac18
elodierubert802 <elodierubert802@gmail.com>
2025-03-14 14:12:20 +0000

    Add 8.ts

---
+++ src/8.ts
@@ -0,0 +1,11 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-github-project',
+  templateUrl: './github-project.component.html',
+  styleUrls: ['./github-project.component.css']
+})
+export class GithubProjectComponent {
+  constructor() {}
+}
